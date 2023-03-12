3623ab5bc9cf14573ca0cc7828b771dfc1ba789d
Kostiantyn Doichev <kostiantyn.doichev@panenco.com>
2023-03-12 17:29:09 +0100

    feat: button component

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,17 @@
-/** @type {import('tailwindcss').Config} */
+/** @type {import("tailwindcss").Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				primary: `hsl(var(--primary-color)/var(--tw-bg-opacity))`,
+				"primary-focused": `hsl(var(--primary-focused)/var(--tw-bg-opacity))`,
+			},
+			backgroundColor: {
+				primary: `hsl(var(--primary-color)/var(--tw-bg-opacity))`,
+				"primary-focused": `hsl(var(--primary-focused)/var(--tw-bg-opacity))`,
+			},
+		},
 	},
 	plugins: [],
-}
+};
