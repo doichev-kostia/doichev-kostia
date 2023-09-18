41a52815e327be7ae57b30fe21c47cdc482033ec
Kostiantyn Doichev <doichev.kostia@gmail.com>
2023-09-18 08:52:39 +0200

    feat: dependency updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
-import { defineConfig } from "astro/config";
+import { defineConfig, sharpImageService } from "astro/config";
 
 // https://astro.build/config
 import tailwind from "@astrojs/tailwind";
 
-// https://astro.build/config
-import image from "@astrojs/image";
 
 // https://astro.build/config
 import sitemap from "@astrojs/sitemap";
@@ -12,7 +10,8 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
 	site: "https://doichevkostia.dev",
-	integrations: [tailwind(), image({
-		serviceEntryPoint: "@astrojs/image/sharp"
-	}), sitemap()]
+	image: {
+		service: sharpImageService(),
+	},
+	integrations: [tailwind(), sitemap()]
 });
