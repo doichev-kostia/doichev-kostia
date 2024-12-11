4d249167839ef4fe1b3c96e34334e875a0e0907e
Kostiantyn Doichev <doichev.kostia@gmail.com>
2024-12-11 09:39:33 +0100

    some content fixes

--- packages/www/src/content/config.ts
+++ packages/www/src/content/config.ts
@@ -1,7 +1,8 @@
 import { defineCollection, z } from "astro:content";
+import { glob } from 'astro/loaders';
 
 const blogCollection = defineCollection({
-	type: "content",
+	loader: glob({ pattern: '**/[^_]*.md', base: "./src/content/blog" }),
 	schema: z.object({
 		title: z.string(),
 		summary: z.string(),
